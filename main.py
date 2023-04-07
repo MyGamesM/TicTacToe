abada8fad8e00aa975d058454e1912415ceaa71f
MyGamesM <milanmilan.mm55@gmail.com>
2023-04-07 19:32:56 +0200

    mostly finished

---
+++ main.py
@@ -0,0 +1,96 @@
+import os, pygame
+
+os.system("cls")
+
+pygame.init()
+
+screen = pygame.display.set_mode((600, 600))
+clock = pygame.time.Clock()
+running: bool = True
+turn: bool = True
+finished: bool = False
+winner: bool = False
+board: list[list[int]] = [[0 for _ in range(3)] for _ in range(3)]
+GRAY: tuple[int, int, int] = (200, 200, 200)
+
+def draw_board() -> None:
+	a = 37
+	for i in range(4):
+		pygame.draw.line(screen, GRAY, (a + (175 * i), 25), (a + (175 * i), 575), 2)
+		pygame.draw.line(screen, GRAY, (25, a + (175 * i)), (575, a + (175 * i)), 2)
+
+	for i in range(len(board)):
+		for j in range(len(board[i])):
+			match board[j][i]:
+				case 1:
+					draw_x(i, j)
+				case 2:
+					draw_o(i, j)
+
+def draw_x(x: int, y: int) -> None:
+	x, y = x + 1, y + 1
+	x = 48 + (x - 1) * 175
+	y = 48 + (y - 1) * 175
+	a = 150
+	pygame.draw.line(screen, GRAY, (x, y), (x + a, y + a), 2)
+	pygame.draw.line(screen, GRAY, (x + a, y), (x, y + a), 2)
+
+def draw_o(x: int, y: int) -> None:
+	x, y = x + 1, y + 1
+	x, y = x + 123 + ((x - 1) * 175), y + 123 + ((y - 1) * 175)
+	pygame.draw.circle(screen, GRAY, (x, y), 75, 2)
+
+def draw_end_screen() -> None:
+	pygame.draw.circle(screen, GRAY, (300, 300), 50, 2)
+
+while running:
+	screen.fill("black")
+
+	if finished:
+		draw_end_screen()
+		print("x") if not winner else print("o")
+		running = False
+	else:
+		for i in range(3):
+			if board[i][0] != 0 and board[i][1] != 0 and board[i][2] != 0 and board[i][0] == board[i][1] == board[i][2]:
+				print("a")
+				finished = True
+				winner = board[i][0] % 2 == 0
+
+			if board[0][i] != 0 and board[1][i] != 0 and board[2][i] != 0 and board[0][i] == board[1][i] == board[2][i]:
+				print("b")
+				finished = True
+				winner = board[0][i] % 2 == 0
+
+		if board[0][0] != 0 and board[1][1] != 0 and board[2][2] != 0 and board[0][0] == board[1][1] == board[2][2]:
+			print("c")
+			finished = True
+			winner = board[0][0] % 2 == 0
+
+		if board[0][2] != 0 and board[1][1] != 0 and board[2][0] != 0 and board[0][2] == board[1][1] == board[2][0]:
+			print("d")
+			finished = True
+			winner = board[0][0] % 2 == 0
+
+		draw_board()
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			if event.button == 1:
+				x, y = pygame.mouse.get_pos()
+				x, y = int((x - 40) / 170), int((y - 40) / 170)
+				if x < 3 and y < 3 and board[y][x] == 0:
+					if turn:
+						board[y][x] = 1
+						turn = False
+					else:
+						board[y][x] = 2
+						turn = True
+
+	pygame.display.flip()
+
+	clock.tick(60)
+
+pygame.quit()
